ccb09ff00007b17e02fba7302d5c382cb29bc4d4
dhilipsiva <dhilipsiva@gmail.com>
2016-06-29 22:43:42 +0530

    Minor improvement on previous commit

--- scrum.py
+++ scrum.py
@@ -18,6 +18,7 @@ from os.path import expanduser, join
 import click
 
 FORMAT_VERSION = 1
+SCRUM_PATH = join(expanduser("~"), ".scrum")
 
 
 @click.command()
@@ -44,6 +45,5 @@ def setup():
     api_key = input()
     dot_scrum["api_key"] = api_key
     dot_scrum["format_version"] = FORMAT_VERSION
-    dot_scrum_path = join(expanduser("~"), ".scrum")
-    dot_scrum_file = open(dot_scrum_path, "w")
+    dot_scrum_file = open(SCRUM_PATH, "w")
     dot_scrum_file.write(dumps(dot_scrum))
