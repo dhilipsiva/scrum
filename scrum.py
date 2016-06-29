fce9470dc4fbcdb00460692f2839d82a446dca78
dhilipsiva <dhilipsiva@gmail.com>
2016-06-29 22:14:39 +0530

    Add sample script

---
+++ scrum.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: scrum.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-06-29
+"""
+
+import click
+
+
+@click.command()
+def scrum():
+    """Example script."""
+    click.echo('Hello World!')
---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: setup.py
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2016-06-29
+"""
+
+from setuptools import setup
+
+setup(
+    name='scrum',
+    url='https://github.com/dhilipsiva/scrum',
+    version='0.0.1',
+    py_modules=['scrum'],
+    author='dhilipsiva',
+    author_email='dhilipsiva@gmail.com',
+    install_requires=[
+        'Click',
+        'PyGithub'
+    ],
+    entry_points='''
+        [console_scripts]
+        scrum=scrum:scrum
+    ''',
+)
